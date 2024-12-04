1d0509ce206c8f9f4c0219fbe97bb63c92214b0d
Park Yoonseo <patpark97@gmail.com>
2024-12-04 16:07:19 +0900

    feat: [박윤서] Player 전체 리스트 조회 [SBwDx]

--- src/main/java/com/example/demo/player/controller/PlayerController.java
+++ src/main/java/com/example/demo/player/controller/PlayerController.java
@@ -7,6 +7,7 @@ import com.example.demo.player.controller.response_form.PlayerListResponseForm;
 import com.example.demo.player.entity.Player;
 import com.example.demo.player.service.response.PlayerCreateResponse;
 import com.example.demo.player.service.response.PlayerListResponse;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -39,4 +40,11 @@ public class PlayerController {
         return foundPlayer;
     }
 
+    @GetMapping("/list")
+    public List<Player> listPlayer() {
+        log.info("listPlayer() called!");
+
+        List<Player> responseList = playerService.listPlayer();
+        return responseList;
+    }
 }
