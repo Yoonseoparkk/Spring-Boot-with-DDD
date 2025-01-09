e55a99ed67bb6973b61ae338b1d15f8a290d18db
Park Yoonseo <patpark97@gmail.com>
2025-01-09 17:15:19 +0900

    feat: [박윤서] Board 도메인 및 BoardList 구현 [SBwDx]

---
+++ src/main/java/com/example/demo/board/service/BoardServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.board.service;
+
+import com.example.demo.account.repository.AccountRepository;
+import com.example.demo.account_profile.repository.AccountProfileRepository;
+import com.example.demo.board.entity.Board;
+import com.example.demo.board.repository.BoardRepository;
+import com.example.demo.board.service.request.ListBoardRequest;
+import com.example.demo.board.service.response.ListBoardResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    final private BoardRepository boardRepository;
+    final private AccountRepository accountRepository;
+    final private AccountProfileRepository accountProfileRepository;
+
+    @Override
+    public ListBoardResponse list(ListBoardRequest request) {
+        // PageRequest 생성 (0부터 시작하는 페이지 인덱스를 위해 page - 1 을 사용)
+        PageRequest pageRequest = PageRequest.of(request.getPage() - 1, request.getPerPage());
+
+        // 페이징된 게시글 목록을 가져오기
+        Page<Board> boardPage = boardRepository.findAllWithWriter(pageRequest);
+
+        // ListBoardResponse 객체로 변환하여 반환
+        return new ListBoardResponse(boardPage.getContent(), boardPage.getTotalElements(), boardPage.getTotalPages());
+    }
+}
